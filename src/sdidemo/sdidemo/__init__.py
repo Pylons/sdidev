3884c27ed21799f3ced8855872a5402dc9027f6d
Balazs Ree <ree@greenfinity.hu>
2012-11-20 15:58:33 +0100

    Wire in static resources from slickgrid. No-egg package is checked out with mr.developer.

--- src/sdidemo/sdidemo/__init__.py
+++ src/sdidemo/sdidemo/__init__.py
@@ -9,4 +9,14 @@ def main(global_config, **settings):
     config.include('substanced')
     config.add_catalog_index('title', 'field', 'sdidemo')
     config.scan()
+
+    slickgrid_static_path = settings.get('slickgrid.static_path', None)
+    if slickgrid_static_path is None:
+        raise RuntimeError, 'You must specify the slickgrid.static_path variable' \
+                            ' to point to your slickgrid sources. Or: use the ini' \
+                            ' provided by the buildout, which automates this.'
+
+    config.add_static_view('/sg-static', slickgrid_static_path,
+        cache_max_age=60 * 60 * 24 * 365)
+
     return config.make_wsgi_app()
