8a6da86f56d10a42ac8677982983d3f3d4f10a14
Paul Everitt <paul@agendaless.com>
2012-11-28 16:21:04 -0500

    Add some more screenshots, document tab ordering.

--- src/sdidemo/sdidemo/views.py
+++ src/sdidemo/sdidemo/views.py
@@ -14,10 +14,11 @@ from .resources import DocumentSchema
 #
 @view_config(
     renderer='templates/splash.pt',
-    )
+)
 def splash_view(request):
-    manage_prefix = request.registry.settings.get('substanced.manage_prefix', 
-                                                  '/manage')
+    manage_prefix = request.registry.settings.get(
+        'substanced.manage_prefix',
+        '/manage')
     return {'manage_prefix': manage_prefix}
 
 #
@@ -26,12 +27,13 @@ def splash_view(request):
 @view_config(
     context=Document,
     renderer='templates/document.pt',
-    )
+)
 def document_view(context, request):
     return {'title': context.title,
             'body': context.body,
-            'master': get_renderer('templates/master.pt').implementation(),
-           }
+            'master': get_renderer(
+                'templates/master.pt').implementation(),
+    }
 
 #
 #   SDI "add" view for documents
@@ -39,11 +41,11 @@ def document_view(context, request):
 @mgmt_view(
     context=IFolder,
     name='add_document',
-    tab_title='Add Document', 
-    permission='sdi.add-content', 
+    tab_title='Add Document',
+    permission='sdi.add-content',
     renderer='substanced.sdi:templates/form.pt',
     tab_condition=False,
-    )
+)
 class AddDocumentView(FormView):
     title = 'Add Document'
     schema = DocumentSchema()
@@ -54,5 +56,38 @@ class AddDocumentView(FormView):
         name = appstruct.pop('name')
         document = registry.content.create('Document', **appstruct)
         self.context[name] = document
-        return HTTPFound(self.request.mgmt_path(self.context, '@@contents'))
+        return HTTPFound(
+            self.request.mgmt_path(self.context, '@@contents'))
 
+from substanced.sdi import LEFT, RIGHT
+
+@mgmt_view(name='tab_1', tab_title='Tab 1',
+           renderer='templates/tab_1.pt'
+)
+def tab_1(context, request):
+    return {}
+
+
+@mgmt_view(name='tab_2', tab_title='Tab 2',
+           renderer='templates/tab_1.pt',
+           tab_before='tab_1')
+def tab_2(context, request):
+    return {}
+
+
+@mgmt_view(name='tab_3', tab_title='Tab 3',
+           renderer='templates/tab_1.pt', tab_after=LEFT)
+def tab_3(context, request):
+    return {}
+
+
+@mgmt_view(name='tab_4', tab_title='Tab 4',
+           renderer='templates/tab_1.pt')
+def tab_4(context, request):
+    return {}
+
+
+@mgmt_view(name='tab_5', tab_title='Tab 5',
+           renderer='templates/tab_1.pt')
+def tab_5(context, request):
+    return {}
