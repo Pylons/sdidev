f94766f8d3adc55a663d0d5131f86e0540ca6710
Chris McDonough <chrism@plope.com>
2012-11-29 00:41:47 -0500

    match substanced

--- src/sdidemo/sdidemo/views.py
+++ src/sdidemo/sdidemo/views.py
@@ -59,7 +59,7 @@ class AddDocumentView(FormView):
         return HTTPFound(
             self.request.mgmt_path(self.context, '@@contents'))
 
-from substanced.sdi import LEFT, RIGHT
+from substanced.sdi import LEFT, RIGHT, MIDDLE
 
 @mgmt_view(name='tab_1', tab_title='Tab 1',
            renderer='templates/tab_1.pt'
@@ -76,18 +76,19 @@ def tab_2(context, request):
 
 
 @mgmt_view(name='tab_3', tab_title='Tab 3',
-           renderer='templates/tab_1.pt', tab_after=LEFT)
+           renderer='templates/tab_1.pt', tab_near=RIGHT)
 def tab_3(context, request):
     return {}
 
 
 @mgmt_view(name='tab_4', tab_title='Tab 4',
-           renderer='templates/tab_1.pt')
+           renderer='templates/tab_1.pt', tab_near=LEFT)
 def tab_4(context, request):
     return {}
 
 
 @mgmt_view(name='tab_5', tab_title='Tab 5',
-           renderer='templates/tab_1.pt')
+           renderer='templates/tab_1.pt', tab_near=LEFT)
 def tab_5(context, request):
     return {}
+
